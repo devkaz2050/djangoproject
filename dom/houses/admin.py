eaff362001600f1ae4843e74e5f5d632292e4e3c
altynbek2003 <altynbek2003"gmail.com>
2022-09-01 11:38:07 +0600

    commit

---
+++ dom/houses/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from houses.models import House
+
+# Register your models here.
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display=["name","price","active"]
+    list_filter=["active"]
